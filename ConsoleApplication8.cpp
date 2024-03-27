4481ba150ee53bba1fddf168bf63a5ea1b297d89
MariaXax <165185008+MariaXax@users.noreply.github.com>
2024-03-27 20:42:58 +0300

    программа, рисующая треугольники
    (только прямоугольные)

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, kk = 0;
+	cin >> n;
+	cout << "*" << endl;
+	for (int i = 1; i < n; i++)
+	{
+		cout << "*";
+		for (int k = 0; k < i; k++)
+		{
+			cout << " ";
+		}
+		cout << "*" << endl;
+	}
+	for (int i = 0; i <= n + 1; i++)
+	{
+		cout << "*";
+	}
+}
